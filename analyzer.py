9bafacaf3bcb616e88572d4ce5bdf889b5b2cf99
Chibunin <chibunin1988@gmail.com>
2020-02-16 00:06:59 +0500

    WIP. Pull issues. Tests

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,59 @@
+import unittest
+import sys
+import io
 import argparse
+import datetime
 import logging
 import json
 from urllib.request import Request, urlopen
+from contextlib import contextmanager
 
 GH_API_HOST = 'https://api.github.com'
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
+
+
+@contextmanager
+def __test_stdout():
+    old_stdout, new_stdout = sys.stdout, io.StringIO()
+    try:
+        sys.stdout = new_stdout
+        yield sys.stdout
+    finally:
+        sys.out = old_stdout
+
+
+class TestViewLevelFunctions(unittest.TestCase):
+
+    def test_print_title(self):
+        """Test prints out formatted lines as a block's title"""
+        with self.assertRaises(TypeError):
+            print_title()
+
+        with globals()['__test_stdout']() as out:
+            print_title('!')
+        self.assertEqual(out.getvalue(), '===\n!\n')
+
+    def test_print_list(self):
+        """Test prints out formatted lines as a block with a title"""
+        with self.assertRaises(TypeError):
+            print_list()
+
+        with globals()['__test_stdout']() as out:
+            print_list([])
+        self.assertEqual(out.getvalue(), '===\n...\n')
+
+        with globals()['__test_stdout']() as out:
+            print_list(['!', '!'], title='>')
+        self.assertEqual(out.getvalue(), '===\n>\n!\n!\n')
+
+
+def __run_tests():
+    suite = unittest.TestSuite()
+    suite.addTest(TestViewLevelFunctions('test_print_title'))
+    suite.addTest(TestViewLevelFunctions('test_print_list'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
 
 
 def print_title(title):
@@ -19,8 +67,12 @@ def print_list(_list, title='...'):
 
 
 def print_as_table_2c(rows, title='...'):
-    # todo empty rows
     print_title(title)
+
+    if not rows:
+        print('Empty')
+        return
+
     col_size_list = [0, 0]
     prepared_rows = []
     for row in rows:
@@ -64,6 +116,7 @@ def is_repo_url(_url) -> bool:
 def get_repo_full_name(_url) -> str:
     """
     :param _url: string like 'https://github.com/facebookresearch/cparser'
+    :return: repo's full name like 'facebookresearch/cparser'
     """
     if is_repo_url(_url):
         return '/'.join(_url.split('/')[3:5])
@@ -84,19 +137,27 @@ def get_repo_contributors(repo):
     :return:
     """
     # todo error 403 on 'https://api.github.com/repos/torvalds/linux/contributors'
-    res = get_api_response('/repos/{}/contributors?anon=1&per_page=30'.format(repo))
+    res = get_api_response('/repos/{}/contributors?anon=1&per_page=30'.format(repo))  # todo sort
     return [(contr['login'] if 'login' in contr else '-', contr['contributions'])
             for contr in res]
 
 
-def get_repo_pull_requests(repo):
+def get_repo_data(repo, category: str, eld: int):
+    """
+    :param category: a string like 'pulls' or 'issues'
+    """
     # todo param base (branch)
-    res = get_api_response('/repos/{}/pulls?per_page=30&state=all'.format(repo))
+    res = get_api_response('/repos/{}/{}?per_page=30&state=all'.format(repo, category))
     # todo process pages
-    prs = {'open': [], 'closed': []}
+    prs = {'open': [], 'closed': [], 'old': []}
+    now = datetime.datetime.today()  # todo user date arg
+    limit = datetime.timedelta(eld)
     for pr in res:
         prs[pr['state']].append(pr)
-    return len(prs['open']), len(prs['closed'])
+        pr_created = datetime.datetime.strptime(pr['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+        if pr['state'] == 'open' and now - pr_created >= limit:
+            prs['old'].append(pr)
+    return len(prs['open']), len(prs['closed']), len(prs['old'])
 
 
 def make_full_analysis(_url) -> bool:
@@ -107,11 +168,15 @@ def make_full_analysis(_url) -> bool:
     if not repo:
         return False
     print_as_table_2c(get_repo_contributors(repo), 'Contributors')
-    print_list(['Open {}, closed {}'.format(*get_repo_pull_requests(repo))], 'Pull requests')
+    print_list(['Open {}, closed {}, old {}'.format(*get_repo_data(repo, 'pulls', 30))], 'Pull requests')
+    print_list(['Open {}, closed {}, old {}'.format(*get_repo_data(repo, 'issues', 14))], 'Issues')
     return True
 
 
 def router(args):
+    if getattr(args, 'maintenance', False):
+        __run_tests()
+        return
     if getattr(args, 'org', False):
         logger.debug('The organization: {}'.format(args.org))
         get_repos(args.org)
@@ -127,6 +192,8 @@ def main():
     arg_parser = argparse.ArgumentParser(description='GITHUB repo analyzer.')
     arg_parser.add_argument('--org', help='Show a list of repos by an organization')
     arg_parser.add_argument('--url', help='Show a full analysis')
+    arg_parser.add_argument('--self-checking',
+                            dest='maintenance', help='Run a tool\'s maintenance', action='store_true')
     # todo take token for more request amount (Rate problem)
     # todo debug logging
     args = arg_parser.parse_args()
