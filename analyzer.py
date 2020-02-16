0b1028a16f805e585300a32c8fa037d1b4a8bc82
Chibunin <chibunin1988@gmail.com>
2020-02-16 19:50:14 +0500

    WIP. User filters. Pages

--- analyzer.py
+++ analyzer.py
@@ -6,11 +6,13 @@ import datetime
 import logging
 import json
 from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 from contextlib import contextmanager
 
 GH_API_HOST = 'https://api.github.com'
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
+notifications = set()
 
 
 @contextmanager
@@ -117,19 +119,37 @@ def print_as_table_2c(rows, title='...'):
              prepared_rows))
 
 
+def add_notification(msg):
+    global notifications
+    notifications.add(msg)
+
+
 def get_api_response(resource):
     req = Request('{}{}'.format(GH_API_HOST, resource))
     req.add_header('Accept', 'application/vnd.github.v3+json')
-    res = urlopen(req)
-    # TODO Rate problem
-    return json.loads(res.read().decode('utf-8'))
+    # req.add_header('Authorization', 'token ...')
+    try:
+        # TODO Rate problem (if you need to run the analyzer many times in hour)
+        res = urlopen(req)
+        links = res.getheader('Link')
+    except HTTPError:
+        # todo GH messages
+        # todo error 403 on 'https://api.github.com/repos/torvalds/linux/contributors' (contributor list is too large)
+        add_notification('The API not available. Try again later.')
+        return [], ''
+    return json.loads(res.read().decode('utf-8')), links
 
 
 def is_repo_url(_url) -> bool:
     """
     :param _url: string like 'https://github.com/facebookresearch/cparser'
     """
-    res = urlopen(Request(_url))
+    try:
+        res = urlopen(Request(_url))
+    except HTTPError:
+        add_notification('The API not available. Try again later.')
+        return False
+
     if res.getheader('Server') == 'GitHub.com' \
             and res.getheader('Status') == '200 OK':
         logger.debug('Input URL is valid')
@@ -150,7 +170,7 @@ def get_repo_full_name(_url) -> str:
 
 def get_repos(org):
     """Getting repositories by an organization"""
-    res = get_api_response('/orgs/{}/repos'.format(org))
+    res, _ = get_api_response('/orgs/{}/repos'.format(org))
     logger.debug('Repos: {}'.format(res))
     print_list([repo['full_name'] for repo in res], 'Repos:')
 
@@ -161,39 +181,69 @@ def get_repo_contributors(repo):
     :param repo: string like 'facebookresearch/cparser'
     :return:
     """
-    # todo error 403 on 'https://api.github.com/repos/torvalds/linux/contributors'
-    res = get_api_response('/repos/{}/contributors?anon=1&per_page=30'.format(repo))  # todo sort
+    res, _ = get_api_response('/repos/{}/contributors?anon=1&per_page=30'.format(repo))
     return [(contr['login'] if 'login' in contr else '-', contr['contributions'])
             for contr in res]
 
 
-def get_repo_data(repo, category: str, eld: int):
+def get_repo_data(repo, category: str, eld: int, branch=None, b_date=None, e_date=None):
     """
     :param category: a string like 'pulls' or 'issues'
     """
-    # todo param base (branch)
-    res = get_api_response('/repos/{}/{}?per_page=30&state=all'.format(repo, category))
-    # todo process pages
+    additional_params = ''
+    if branch and category == 'pulls':
+        additional_params = '&base={}'.format(branch)
+
+    page = 1
+    is_run = True
+    pages = []
+    while is_run:
+        # API do not have special parameters for filtering by dates what we need
+        res, links = get_api_response('/repos/{}/{}?page={}&per_page=30&state=all{}'.
+                                      format(repo, category, page, additional_params if additional_params else ''))
+        pages += res
+        try:
+            links.index('rel="next"')
+            page += 1
+        except ValueError:
+            is_run = False
+
     prs = {'open': [], 'closed': [], 'old': []}
-    now = datetime.datetime.today()  # todo user date arg
+    # todo user date arg
+    date_from = b_date if b_date else datetime.datetime(1970, 1, 1)
+    date_to = e_date if e_date else datetime.datetime.today()
     limit = datetime.timedelta(eld)
-    for pr in res:
-        prs[pr['state']].append(pr)
+    for pr in pages:
         pr_created = datetime.datetime.strptime(pr['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-        if pr['state'] == 'open' and now - pr_created >= limit:
+        if date_from > pr_created >= date_to:
+            continue
+
+        pr_closed = datetime.datetime.strptime(pr['closed_at'], '%Y-%m-%dT%H:%M:%SZ') if pr['closed_at'] else None
+        is_closed = False
+        if pr_closed and pr_closed <= date_to:
+            is_closed = True
+
+        if is_closed:
+            prs['closed'].append(pr)
+        else:
+            prs['open'].append(pr)
+
+        if not is_closed and date_to - pr_created >= limit:
             prs['old'].append(pr)
+
     return len(prs['open']), len(prs['closed']), len(prs['old'])
 
 
-def make_full_analysis(_url) -> bool:
+def make_full_analysis(_url, branch: str, b_date: datetime.datetime, e_date: datetime.datetime) -> bool:
     """
     :param _url: string like 'https://github.com/facebookresearch/cparser'
     """
     repo = get_repo_full_name(_url)
     if not repo:
         return False
-    print_as_table_2c(get_repo_contributors(repo), 'Contributors')
-    print_list(['Open {}, closed {}, old {}'.format(*get_repo_data(repo, 'pulls', 30))], 'Pull requests')
+    # print_as_table_2c(get_repo_contributors(repo), 'Contributors')
+    print_list(['Open {}, closed {}, old {}'.
+               format(*get_repo_data(repo, 'pulls', 30, branch, b_date, e_date))], 'Pull requests')
     print_list(['Open {}, closed {}, old {}'.format(*get_repo_data(repo, 'issues', 14))], 'Issues')
     return True
 
@@ -208,21 +258,33 @@ def router(args):
         return
     if getattr(args, 'url', False):
         logger.debug('The repo\'s URL: {}'.format(args.url))
-        if make_full_analysis(args.url):
+        if make_full_analysis(args.url, args.branch, args.b_date, args.e_date):
             return
     print('Nothing')
 
 
 def main():
+    def valid_date(s):
+        try:
+            return datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            msg = "Not a valid date: '{0}'.".format(s)
+            raise argparse.ArgumentTypeError(msg)
+
     arg_parser = argparse.ArgumentParser(description='GITHUB repo analyzer.')
     arg_parser.add_argument('--org', help='Show a list of repos by an organization')
     arg_parser.add_argument('--url', help='Show a full analysis')
+    arg_parser.add_argument('--branch', help='Specify a branch. There is a default: master')
+    arg_parser.add_argument('--beginning-date', type=valid_date, dest='b_date',
+                            help='Specify a beginning date like \'YYYY-MM-DD HH:MM:SS\'. It is an optional')
+    arg_parser.add_argument('--end-date', type=valid_date, dest='e_date',
+                            help='Specify an end date like \'YYYY-MM-DD HH:MM:SS\'. It is an optional')
     arg_parser.add_argument('--self-checking',
                             dest='maintenance', help='Run a tool\'s maintenance', action='store_true')
-    # todo take token for more request amount (Rate problem)
     # todo debug logging
     args = arg_parser.parse_args()
     router(args)
+    print_list(notifications, 'System notifications') if notifications else None
 
 
 if __name__ == '__main__':
